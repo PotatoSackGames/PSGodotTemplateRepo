73ee894221bcd1cb1a59c0b4d5950cb9a7e667bc
Kyle Szklenski <kyle.szklenski@gmail.com>
2023-07-14 11:58:53 -0400

    Update game state manager for leet usage

--- toolkit/singletons/GameStateManager.gd
+++ toolkit/singletons/GameStateManager.gd
@@ -1,21 +1,33 @@
+@tool
 extends Node
 
-var _game_state = {}
-
 # Hold onto game state; this can be loaded with SaverLoader, and can be manipulated
-# with set_item_state. Retrieve it if you wish with get_item_state.
+# with set_item_state(property_name, property_value). Retrieve it if you wish by simply using GSM.property_name.
 
-# has_item_state will check if a specific state exists; if it does not, get_item_state
-# will return null, SO BE CAREFUL.
+# This absolutely hijacks the Godot property system and makes it so whenever you set the game state with
+# GameStateManager.set_item_state(property_name, property_value), you can then just call GSM.property_name and it'll return the value to you.
+# Mind that if you do not set the value first, it will throw an error!
 
+# Note I have not solved how to make this work with a setting just yet due to some internal safety checks that Godot has.
+
+var _game_state : Array[Dictionary]
+
+func _enter_tree():
+	_game_state = get_property_list()
+	
+func _get(property):
+	for item in _game_state:
+		if item.name == property:
+			if item.has("value"):
+				return item.value
+			else:
+				return item
+	
+	assert(false, "Property " + property + " has not had set_item_state called on it initially")
+	return null
+	
 func set_item_state(item_id : String, state : Variant):
-	_game_state[item_id] = state
+	_game_state.append({"name": item_id, "type": typeof(state), "value": state })
 
 func has_item_state(item_id):
 	return _game_state.has(item_id)
-	
-func get_item_state(item_id):
-	if has_item_state(item_id):
-		return _game_state[item_id]
-		
-	return null
