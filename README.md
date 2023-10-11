68fe64619b57d0c1e710b497c90ebf87e1454e1f
Kyle Szklenski <kyle.szklenski@gmail.com>
2023-10-11 16:44:57 -0400

    Add mouseover sfx component

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Template repository for Godot 4.x 2D games!
 
 All you have to do is hit the button to use the template. It should be in the top right! All features are documented, but will include tutorials here soon.
+
+Credit to Kenney.nl for the sound effect(s)!
--- test.gd
+++ test.gd
@@ -1,22 +1,27 @@
 extends Node2D
 
+var _is_running := false
+
 # Demo scene for the special effects functionality, and music if you want to add a FunkStream and EDMStream to the Music singleton
 
-func _ready() -> void:
-#	Music.change_music_immediate(Music.FunkStream)
-	await get_tree().create_timer(1.0).timeout
-	await try_effect(preload("res://toolkit/nodes/effects/squash.tscn"), $Squash)
-	await try_effect(preload("res://toolkit/nodes/effects/stretch.tscn"), $Stretch)
-	var effects : Array[PackedScene] = [preload("res://toolkit/nodes/effects/squash.tscn"), preload("res://toolkit/nodes/effects/stretch.tscn")]
-	var effect_times : Array[float] = [.2, .2]
-	await try_multiple_effects(effects, effect_times, $SquashThenStretch)
-	await try_effect(preload("res://toolkit/nodes/effects/explosion.tscn"), $ParticlesTest, 1.0)
-	await try_effect(preload("res://toolkit/nodes/effects/damage.tscn"), $DamageEffect, .5)
-	await try_effect(preload("res://toolkit/nodes/effects/round_hit.tscn"), $RoundHit, .60)
-	
-	effects = [preload("res://toolkit/nodes/effects/round_hit.tscn"), preload("res://toolkit/nodes/effects/explosion.tscn"), preload("res://toolkit/nodes/effects/damage.tscn")]
-	await try_multiple_parallel_effects(effects, 1.0, $DamageCombo)
-	await try_effect(preload("res://toolkit/nodes/effects/hit_stop.tscn"), $HitStop, .71)
+func _on_run_effects_pressed() -> void:
+	if not _is_running:
+		_is_running = true
+	#	Music.change_music_immediate(Music.FunkStream)
+		await get_tree().create_timer(1.0).timeout
+		await try_effect(preload("res://toolkit/nodes/effects/squash.tscn"), $Squash)
+		await try_effect(preload("res://toolkit/nodes/effects/stretch.tscn"), $Stretch)
+		var effects : Array[PackedScene] = [preload("res://toolkit/nodes/effects/squash.tscn"), preload("res://toolkit/nodes/effects/stretch.tscn")]
+		var effect_times : Array[float] = [.2, .2]
+		await try_multiple_effects(effects, effect_times, $SquashThenStretch)
+		await try_effect(preload("res://toolkit/nodes/effects/explosion.tscn"), $ParticlesTest, 1.0)
+		await try_effect(preload("res://toolkit/nodes/effects/damage.tscn"), $DamageEffect, .5)
+		await try_effect(preload("res://toolkit/nodes/effects/round_hit.tscn"), $RoundHit, .60)
+		
+		effects = [preload("res://toolkit/nodes/effects/round_hit.tscn"), preload("res://toolkit/nodes/effects/explosion.tscn"), preload("res://toolkit/nodes/effects/damage.tscn")]
+		await try_multiple_parallel_effects(effects, 1.0, $DamageCombo)
+		await try_effect(preload("res://toolkit/nodes/effects/hit_stop.tscn"), $HitStop, .71)
+		_is_running = false
 #	await get_tree().create_timer(5.0).timeout
 #	await Music.change_music(Music.EDMStream, 3.0)
 #	await Music.change_music(Music.EDMStream, 3.0)
